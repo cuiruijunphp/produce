9699d5205f4c7113a9b807ad650db4637f2041db
cuirj <cuirj@mingyuanyun.com>
2017-08-15 22:50:10 +0800

    111

--- application/controllers/Commons.php
+++ application/controllers/Commons.php
@@ -47,7 +47,7 @@ class Commons extends MY_Controller {
 			$access_token_res = $this->access_token->get_one($where,' expire_time desc');
 		}else{
 			//如果没有的话,就新建一个账户,然后写入表
-			$uid = create_guid();
+			$uid = $this->create_guid();
 			$type = 1;
 			//1-买家，2-商户
 			if($type == 1){
